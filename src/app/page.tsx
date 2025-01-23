841536fe59f171e1c8fe1e4c850f9d75cea45a29
ReapingHunter <manalilimarlex@gmail.com>
2025-01-23 17:55:23 +0800

    feat: add winner calculation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,39 @@ import { useState } from "react";
 
 
 export default function Board() {
+  const [xTurn, setXTurn] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+  
   const handleClick = (x) => {
+    if(squares[x] || calculateWinner(squares))
+      return
+
     const nextSquares = squares.slice()
-    nextSquares[x] = 'X'
+    nextSquares[x] = xTurn ? 'X' : 'O'
     setSquares(nextSquares)
+    setXTurn(!xTurn)
   }
 
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    
+    for(let x = 0; x < lines.length; x++){
+      const [a, b, c] = lines[x]
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+        return squares[a]
+    }
+    return null
+  }
   return (
     <div className="items-center justify-items-center">
       <div>Status</div>
