178de3d55526420a29b01ff77ba0632aabd2b36e
ReapingHunter <manalilimarlex@gmail.com>
2025-01-23 17:42:05 +0800

    fix: div taking too much height

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,7 @@
-
+import { useState } from "react";
 export const Square = ({value, onSquareClick}) => {
   return(
-    <button className="bg-transparent border-2 py-2 px-4 font-bold" onClick={onSquareClick}>
+    <button className="bg-transparent border p-0 font-bold w-[34px] h-[34px] float-left" onClick={onSquareClick}>
       {value}
     </button>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,31 @@ import { useState } from "react";
 
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  const handleClick = (x) => {
+    const nextSquares = squares.slice()
+    nextSquares[x] = 'X'
+    setSquares(nextSquares)
+  }
+
   return (
     <div className="items-center justify-items-center">
       <div>Status</div>
       <div>
-        <Square value={1} onSquareClick={()=>{}}></Square>
-        <Square value={2} onSquareClick={()=>{}}></Square>
-        <Square value={3} onSquareClick={()=>{}}></Square>
+        <Square value={squares[0]} onSquareClick={()=>{handleClick(0)}}></Square>
+        <Square value={squares[1]} onSquareClick={()=>{handleClick(1)}}></Square>
+        <Square value={squares[2]} onSquareClick={()=>{handleClick(2)}}></Square>
       </div>
       <div>
-        <Square value={4} onSquareClick={()=>{}}></Square>
-        <Square value={5} onSquareClick={()=>{}}></Square>
-        <Square value={6} onSquareClick={()=>{}}></Square>
+        <Square value={squares[3]} onSquareClick={()=>{handleClick(3)}}></Square>
+        <Square value={squares[4]} onSquareClick={()=>{handleClick(4)}}></Square>
+        <Square value={squares[5]} onSquareClick={()=>{handleClick(5)}}></Square>
       </div>
       <div>
-        <Square value={7} onSquareClick={()=>{}}></Square>
-        <Square value={8} onSquareClick={()=>{}}></Square>
-        <Square value={9} onSquareClick={()=>{}}></Square>
+        <Square value={squares[6]} onSquareClick={()=>{handleClick(6)}}></Square>
+        <Square value={squares[7]} onSquareClick={()=>{handleClick(7)}}></Square>
+        <Square value={squares[8]} onSquareClick={()=>{handleClick(8)}}></Square>
       </div>
     </div>
   );
